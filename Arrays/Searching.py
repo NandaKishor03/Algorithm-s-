e97de2fca3185a750bd405c2473dead476e726fd
NandaKishor03 <nandakishor6772@gmail.com>
2024-08-19 00:51:41 +0530

    First Searching

---
+++ Arrays/Searching.py
@@ -0,0 +1,34 @@
+#   Searching Algorithms are used to locate a specific element in an array.
+
+            #Linear Search
+# Time Complexity: O(n)
+# Description: The simplest search algorithm that checks every element in the array until the desired element is found or the end of the array is reached.
+# Use Case: Works well for unsorted or small arrays.
+
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i  # Return the index of the target element
+    return -1  # Return -1 if the target element is not found
+
+
+
+            #Binary Search
+# Time Complexity: O(log n)
+# Description: A fast search algorithm that works on sorted arrays by repeatedly dividing the search interval in half until the desired element is found.
+# Use Case: Works well for large sorted arrays.
+
+def binary_search(arr, target):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid  # Return the index of the target element
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1  # Return -1 if the target element is not found
+
+        
+    
---
+++ Problem of the Day GFG.py
@@ -0,0 +1,27 @@
+                                    #  GEEKS FOR GEEKS PROBLEM OF THE DAY  #
+
+                # check if a2 is subset of a1 or not
+# a1 = [11, 7, 1, 13, 21, 3, 7, 3]
+# a2 = [11, 3, 7, 1, 7]
+
+# for x in a2:
+#     if x not in a1:
+#         print("No")
+#     else:
+#         a1.remove(x)
+# print("Yes")
+
+
+                # Sum equal to 0
+# lst = [-1,1,-1,1]
+# res = []
+# k = 1 
+# while k <= len(lst):
+#     for i in range(len(lst)-k+1):
+#         arr1 = lst[i:i+k]
+#         if sum(arr1) == 0:
+#             res.append(arr1)
+#     k += 1
+# print(res)
+# print(len(res))
+
